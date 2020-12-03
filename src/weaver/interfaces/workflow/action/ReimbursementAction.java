e1aeab35424b8c81e7a72af2d689cc729bac6bf3
gongchen <1395861613@qq.com>
2020-12-03 11:53:44 +0800

    修改成本中心

--- src/weaver/interfaces/workflow/action/ReimbursementAction.java
+++ src/weaver/interfaces/workflow/action/ReimbursementAction.java
@@ -25,16 +25,32 @@ public class ReimbursementAction extends BaseBean implements Action {
         String requestId = requestInfo.getRequestid();
         //获取主表信息、初始化主表
         Map<String, String> mid=getPropertyMap(requestInfo.getMainTableInfo().getProperty());
-        String BUKRS=Util.null2String(mid.get("lrzx"));  //利润中心
+        String BUKRS=Util.null2String(mid.get("gsdm"));  //公司代码
         String ZOADJ=Util.null2String(mid.get("liucbh")); //流程编号
-        String ZDJLX = "报销"; //票据类型
-        String WRBTR=Util.null2String(mid.get("baoxjehjyb")); //报销合计金额（人民币）
-        String LIFNR = Util.null2String(mid.get("lingkrkhhjzh")); //领款人开户行
-        String BVTYP = Util.null2String(mid.get("yinhzh")); //领款人银行账号
+        String LIFNR = Util.null2String(mid.get("gyshzqrzh")); //供应商或债权人账户
+        String ZDJLX = "报销"; //单据类型
+        String ZLSCH = Util.null2String(mid.get("fkfs"));//付款方式
+        String BVTYP = Util.null2String(mid.get("zhxz")); //账户选择
+        //String WRBTR=Util.null2String(mid.get("baoxjehjyb")); //报销合计金额（人民币）
 
         //获取明细表信息
         DetailTable[] detailtable = requestInfo.getDetailTableInfo().getDetailTable();
-        DetailTable dt = detailtable[0];// 指定明细表
+        DetailTable xm1 = detailtable[1];// 指定明细表2
+        Row[] sxm1 = xm1.getRow();// 当前明细表的所有数据,按行存储
+        Row r1 = sxm1[0];// 指定行
+        Cell co[] = r1.getCell();//行按列存
+        String WRBTR = null;//借款金额
+        String ZJZKK = null;//冲销金额
+        for (int k = 0; k < co.length; k++) {
+            Cell c1 = co[k];// 指定列
+            String name = c1.getName();// 明细字段名称
+            String value = c1.getValue();// 明细字段的值
+            if (name.equals("benccxje")){//冲销借支金额
+                ZJZKK = value;
+            }
+        }
+
+        DetailTable dt = detailtable[0];// 指定明细表1
         Row[] s = dt.getRow();// 当前明细表的所有数据,按行存储
         JSONArray jsonArray = new JSONArray();
         for (int j = 0; j < s.length; j++) {
@@ -45,28 +61,42 @@ public class ReimbursementAction extends BaseBean implements Action {
                 Cell c1 = c[k];// 指定列
                 String name = c1.getName();// 明细字段名称
                 String value = c1.getValue();// 明细字段的值
-                if (name.equals("bb")){
-                    detailtObject.put("WAERS",value); //币别-货币码
+                if(name.equals("hjkm")){
+                    detailtObject.put("HKONT",value);//会计科目
+                }
+                if (name.equals("cbzxin")){
+                    detailtObject.put("KOSTL",value); //成本中心
+                }
+                if (name.equals("nbddxm")){
+                    detailtObject.put("AUFNR",value); //内部订单
                 }
                 if (name.equals("feiysm")){
                     detailtObject.put("BKTXT",value);  //费用说明-凭证抬头文本
                 }
+                if (name.equals("bb")){
+                    detailtObject.put("WAERS",value); //币别-货币码
+                }
+                if (name.equals("baoxjinermb")){
+                    detailtObject.put("WRBTR",value);//费用金额
+                }
+                if (j==s.length-1){
+                    detailtObject.put("ZJZKK",ZJZKK);//冲销金额
+                }else {
+                    detailtObject.put("ZJZKK",0);//冲销金额
+                }
+
             }
-            detailtObject.put("BUKRS",BUKRS);
-            detailtObject.put("ZOADJ",ZOADJ);
-            detailtObject.put("ZDJLX",ZDJLX);
-            detailtObject.put("WRBTR",WRBTR);
-            detailtObject.put("LIFNR",LIFNR);
-            detailtObject.put("BVTYP",BVTYP);
+            detailtObject.put("BUKRS",BUKRS);//公司代码
+            detailtObject.put("ZOADJ",ZOADJ);//OA单据类型
+            detailtObject.put("LIFNR",LIFNR);//供应商或债权人账号
+            detailtObject.put("ZDJLX",ZDJLX);//单据类型
+            detailtObject.put("BVTYP",BVTYP);//账户选择
+            detailtObject.put("ZLSCH",ZLSCH);//付款方式
             jsonArray.add(detailtObject);
             System.out.println(jsonArray);
         }
         jsonObj.put("IT_DATA",jsonArray);
         String shuju = jsonObj.toString();
-        //System.out.println(jsonObj.toJSONString());
-        //ApiUtil apiUtil = new ApiUtil();
-        /*  JSONArray data = apiUtil.invokeApi(jsonObj.toJSONString());*/
-        //JSONObject data = apiUtil.invokeApi(jsonObj.toJSONString());
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), shuju);
         Request request = new Request.Builder()
@@ -87,11 +117,8 @@ public class ReimbursementAction extends BaseBean implements Action {
             e.printStackTrace();
         }
         JSONObject database = JSONObject.parseObject(data);
-        /*String e_code = database.getString("E_CODE");
-        JSONArray et_data = database.getJSONArray("ET_DATA");*/
-        JSONObject one = database.getJSONObject("MT_PaymentDataTransfer_Out_Resp");
-        String e_code = one.getString("E_CODE");
-        String e_msg = one.getString("E_MSG");
+        String e_code = database.getString("E_CODE");
+        String e_msg = database.getString("E_MSG");
         if ("S".equals(e_code)){
             //表示数据传输成功，正常提交
             System.out.println("成功");
