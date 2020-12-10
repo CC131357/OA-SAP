786b2ac7db1f335f36b796d66f1d1173da57110e
gongchen <1395861613@qq.com>
2020-12-10 15:29:32 +0800

    修改外部ECN及扣款接口

--- src/weaver/interfaces/workflow/action/CommonUtil.java
+++ src/weaver/interfaces/workflow/action/CommonUtil.java
@@ -9,7 +9,7 @@ public class CommonUtil {
     private final static String baseUrl="http://10.10.10.31:50000/RESTAdapter/";
     //海关
     public final static String customStateUrl=baseUrl+"OA/S0006CustomsApproveUpdate";
-    //报销（费用报销，业务招待报销，差旅报销）
+    //报销（费用报销，业务招待报销）
     public final static String reimbursementUrl=baseUrl+"OA/S0063PaymentDataTransfer";
     //退货扣款
     public final static String deductioUrl=baseUrl+"OA/S0006CustomsApproveUpdate";
@@ -17,12 +17,14 @@ public class CommonUtil {
     public final static String purchaseUrl=baseUrl+"OA/S0042PRcreate";
     //差旅费用报销
     public final static String travelExpenseUrl=baseUrl+"OA/S0063PaymentDataTransfer";
+    //客户主数据
+    public final static String masterCustomUrl=baseUrl+"OA/S0002CMget";
     public static JSONObject Post(String url, String content) throws IOException,JSONException{
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), content);
         Request request = new Request.Builder()
                 .addHeader("Authorization", "Basic WlBPVVNFUjoxcWF6QFdTWA==")
-                .url(CommonUtil.customStateUrl)
+                .url(url)
                 .post(requestBody)
                 .build();
         Response response = null;
