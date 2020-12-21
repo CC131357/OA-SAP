e8e5781bdc61e8c581c7a339502c46ee84fb0f25
liangwang <2808614706@qq.com>
2020-12-21 15:29:04 +0800

    整理差旅、盘点、信用特批流程的Action代码

--- src/weaver/interfaces/workflow/action/CMCreditApprove.java
+++ src/weaver/interfaces/workflow/action/CMCreditApprove.java
@@ -8,16 +8,22 @@ import weaver.soa.workflow.request.RequestInfo;
 import java.util.Map;
 
 public class CMCreditApprove  extends BaseBean implements Action {
+    final String MESSAGEID = "99999";
+    final String FAILURECODE = "333";
+    CommonUtil util = new CommonUtil();
+
     @Override
     public String execute(RequestInfo requestInfo) {
-        final String MESSAGEID = "99999";
-        final String FAILURECODE = "333";
+        return PostMsg(getPostJsonStr(requestInfo),requestInfo);
+    }
 
+    /**获得需要推送的字符串*/
+    private String getPostJsonStr(RequestInfo requestInfo){
         JSONObject approveJson =new JSONObject();
         //获取主表信息、初始化主表
-        Map<String, String> mid=CommonUtil.getPropertyMap(requestInfo.getMainTableInfo().getProperty());
+        Map<String, String> mainTableInfo = util.getMainTableMap(requestInfo);
 
-        String iDocty = Util.null2String(mid.get("djlx")); //单据类型
+        String iDocty = Util.null2String(mainTableInfo.get("djlx")); //单据类型
         if(iDocty != null && iDocty.equals("0")){   //销售订单
             approveJson.put("I_DOCTY","S");
         } else if(iDocty != null && iDocty.equals("1")){    //交货订单
@@ -27,47 +33,34 @@ public class CMCreditApprove  extends BaseBean implements Action {
             requestInfo.getRequestManager().setMessagecontent("单据类型填写错误！");
             return FAILURECODE;
         }
-        approveJson.put("I_VBELN",Util.null2String(mid.get("djhm"))); //单据号码
-        approveJson.put("I_ZOABH",Util.null2String(mid.get("liucbh"))); //OAI流程编号
+        approveJson.put("I_VBELN",Util.null2String(mainTableInfo.get("djhm"))); //单据号码
+        approveJson.put("I_ZOABH",Util.null2String(mainTableInfo.get("liucbh"))); //OAI流程编号
 
-        String msg = approveJson.toString();
-        System.out.println(msg);
+        return approveJson.toString();
+    }
 
+    /**向SAP推送消息*/
+    private String PostMsg(String msg,RequestInfo requestInfo){
         try{
-            JSONObject database = CommonUtil.Post(CommonUtil.cMCreditApproveUrl,msg);
+            JSONObject database = util.Post(CommonUtil.cMCreditApproveUrl,msg,CommonUtil.authorization);
             String e_code = database.getString("E_CODE");
             String e_msg = database.getString("E_MSG");
             if ("S".equals(e_code)){
                 //表示数据传输成功，正常提交
-                System.out.println("特批成功!" + msg);
+                util.printLog(requestInfo, msg, "特批成功!" + e_msg);
                 return SUCCESS;
-            }else if("E".equals(e_code)){
-                //数据传输失败，则将错误信息返回到页面
-                printLog(requestInfo, "调用客户信用特批解冻申请流程接口，特批失败！,", msg);
+            }else {
+                util.printLog(requestInfo, msg, "调用客户信用特批解冻申请流程接口，特批失败!" + e_msg);
                 requestInfo.getRequestManager().setMessageid(MESSAGEID);
                 requestInfo.getRequestManager().setMessagecontent(e_msg + msg);
                 return FAILURECODE;
-            } else{
-                requestInfo.getRequestManager().setMessageid(MESSAGEID);
-                requestInfo.getRequestManager().setMessagecontent("调用SAP接口失败，返回错误数据1！" + msg);
-                return FAILURECODE;
             }
         } catch (Exception e){
-            e.printStackTrace();
+            util.printLog(requestInfo, msg, "调用SAP接口失败，返回错误数据!" + e.getMessage());
             requestInfo.getRequestManager().setMessageid(MESSAGEID);
-            requestInfo.getRequestManager().setMessagecontent("调用SAP接口失败，返回错误数据2！" + msg);
+            requestInfo.getRequestManager().setMessagecontent("调用SAP接口失败，返回错误数据！" + msg);
             return FAILURECODE;
         }
     }
 
-    /**输出打印信息*/
-    private void printLog(RequestInfo requestInfo, String msg, String returnMsg){
-        writeLog(msg +
-                "创建人【"+requestInfo.getCreatorid()+"】" +
-                "流程id【"+requestInfo.getWorkflowid()+"】" +
-                "流程请求id【"+requestInfo.getRequestid()+"】" +
-                "当前节点【"+requestInfo.getRequestManager().getNodeid()+"】" +
-                "请求标题【"+requestInfo.getRequestManager().getRequestname()+"】"+
-                "请求信息【"+ returnMsg +"】");
-    }
 }
